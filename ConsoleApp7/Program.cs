7ab2f222f2eb36025be05ac4b9d53d36c42a2420
pokachalov@minjust-dnr.ru <pokachalov@minjust-dnr.ru>
2021-10-08 09:22:26 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] inputs = Console.ReadLine().TrimEnd().Split(' ');
+        StringBuilder cond = new StringBuilder(inputs[0]);
+
+        for (int i = 1; i < inputs.Length; i += 2)
+            cond.Replace(inputs[i], inputs[i + 1] == "True" ? "1" : "0");
+
+        cond.Replace("!1", "0");
+        cond.Replace("!0", "1");
+
+        cond.Replace("1&0", "0");
+        cond.Replace("0&1", "0");
+        cond.Replace("1&1", "1");
+        cond.Replace("0&0", "0");
+
+        cond.Replace("1|0", "1");
+        cond.Replace("0|1", "1");
+        cond.Replace("1|1", "1");
+        cond.Replace("0|0", "0");
+
+        cond.Replace("1!=0", "1");
+        cond.Replace("0!=1", "1");
+        cond.Replace("1!=1", "0");
+        cond.Replace("0!=0", "0");
+
+        cond.Replace("1=0", "0");
+        cond.Replace("0=1", "0");
+        cond.Replace("1=1", "1");
+        cond.Replace("0=0", "1");
+
+        Console.WriteLine(cond.ToString().Trim() == "1" ? "True" : "False");
+    }
+}
+
+
+
---
+++ ConsoleApp7/XmlTextReaderImpl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+[Serializable]
+internal class XmlTextReaderImpl : Exception
+{
+    public XmlTextReaderImpl()
+    {
+    }
+
+    public XmlTextReaderImpl(string message) : base(message)
+    {
+    }
+
+    public XmlTextReaderImpl(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+
+    protected XmlTextReaderImpl(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
